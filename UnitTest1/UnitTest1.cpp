94b49aac6b59901c090df8d1e31ea2f2a6ff21e0
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-25 18:43:54 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab8.1(string).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			// Test Count function
+			string testStr1 = "++--==+";
+			int plusCount, minusCount, equalCount;
+			Count(testStr1, plusCount, minusCount, equalCount);
+
+			Assert::AreEqual(3, plusCount);
+			Assert::AreEqual(2, minusCount);
+			Assert::AreEqual(2, equalCount);
+
+			// Test Change function
+			string testStr2 = "++--==+";
+			string modifiedStr = Change(testStr2);
+			Assert::AreEqual(string("*********+"), modifiedStr);
+
+			// Test case with no replacements needed
+			string testStr3 = "+-+=";
+			string modifiedStrNoReplace = Change(testStr3);
+			Assert::AreEqual(string("+-+="), modifiedStrNoReplace);
+		}
+	};
+}
---
+++ lab8.1(string).cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void Count(const string& s, int& plusCount, int& minusCount, int& equalCount)
+{
+    plusCount = minusCount = equalCount = 0;
+
+    for (char c : s) {
+        if (c == '+') plusCount++;
+        else if (c == '-') minusCount++;
+        else if (c == '=') equalCount++;
+    }
+}
+
+string Change(string& s)
+{
+    size_t pos = 0;
+    while ((pos = s.find("++", pos)) != -1) {
+        s.replace(pos, 2, "***");
+        pos += 3;
+    }
+    pos = 0;
+    while ((pos = s.find("--", pos)) != -1) {
+        s.replace(pos, 2, "***");
+        pos += 3;
+    }
+    pos = 0;
+    while ((pos = s.find("==", pos)) != -1) {
+        s.replace(pos, 2, "***");
+        pos += 3;
+    }
+    return s;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    int plusCount, minusCount, equalCount;
+    Count(str, plusCount, minusCount, equalCount);
+
+    cout << "String contains:" << endl;
+    cout << "'+' symbols: " << plusCount << endl;
+    cout << "'-' symbols: " << minusCount << endl;
+    cout << "'=' symbols: " << equalCount << endl;
+
+    string dest = Change(str);
+    cout << "Modified string (param) : " << str << endl;
+    cout << "Modified string (result): " << dest << endl;
+
+    return 0;
+}
